e7eaf97eb59bfbab9f7bfe4ae883984c206a81b0
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-13 13:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, idx|
-    print "#{idx}. #{element}"
+    print "#{idx}. #{element}\n"
   end
 end
